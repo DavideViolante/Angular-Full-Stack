2d514f25197781dc68273acbf88c67e5be803d47
Dave <fungtq3@gmail.com>
2016-12-04 17:23:26 +0100

    Add return type on data service methods. Typescript now requires it.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -11,19 +12,19 @@ export class DataService {
 
   constructor(private http: Http) { }
 
-  getCats() {
+  getCats(): Observable<any> {
     return this.http.get('/cats').map(res => res.json());
   }
 
-  addCat(cat) {
+  addCat(cat): Observable<any> {
     return this.http.post("/cat", JSON.stringify(cat), this.options);
   }
 
-  editCat(cat) {
+  editCat(cat): Observable<any> {
     return this.http.put(`/cat/${cat._id}`, JSON.stringify(cat), this.options);
   }
 
-  deleteCat(cat) {
+  deleteCat(cat): Observable<any> {
     return this.http.delete(`/cat/${cat._id}`, this.options);
   }
   
