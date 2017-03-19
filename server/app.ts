0ddf5a6e15e9ebfa6a5ad0ba1967707123b6ff44
Midhun A Darvin <midhun.darvin@cabotsolutions.com>
2017-03-19 21:20:58 +0530

    Separate out the routes from app.ts

--- server/app.ts
+++ server/app.ts
@@ -4,8 +4,9 @@ import * as morgan from 'morgan';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 
-import config from './config/db'
-import Cat from './models/cat.model'
+import config from './config/db';
+import Cat from './models/cat.model';
+import setRoutes from './routes';
 
 const app = express();
 app.set('port', (process.env.PORT || 3000));
@@ -23,60 +24,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
-
-  // APIs
-  // select all
-  app.get('/cats', (req, res) => {
-    Cat.find({}, (err, docs) => {
-      if (err) { return console.error(err); }
-      res.json(docs);
-    });
-  });
-
-  // count all
-  app.get('/cats/count', (req, res) => {
-    Cat.count((err, count) => {
-      if (err) { return console.error(err); }
-      res.json(count);
-    });
-  });
-
-  // create
-  app.post('/cat', (req, res) => {
-    const obj = new Cat(req.body);
-    obj.save((err, cat) => {
-      if (err) { return console.error(err); }
-      res.status(200).json(cat);
-    });
-  });
-
-  // find by id
-  app.get('/cat/:id', (req, res) => {
-    Cat.findOne({_id: req.params.id}, (err, obj) => {
-      if (err) { return console.error(err); }
-      res.json(obj);
-    });
-  });
-
-  // update by id
-  app.put('/cat/:id', (req, res) => {
-    Cat.findOneAndUpdate({_id: req.params.id}, req.body, (err) => {
-      if (err) { return console.error(err); }
-      res.sendStatus(200);
-    });
-  });
-
-  // delete by id
-  app.delete('/cat/:id', (req, res) => {
-    Cat.findOneAndRemove({_id: req.params.id}, (err) => {
-      if (err) { return console.error(err); }
-      res.sendStatus(200);
-    });
-  });
-
-  app.listen(app.get('port'), () => {
-    console.log('Angular 2 Full Stack listening on port ' + app.get('port'));
-  });
+  setRoutes(app);
 });
 
 export { app };
