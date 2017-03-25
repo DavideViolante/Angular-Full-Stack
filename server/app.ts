418f71f20e1e6f88875c300d7b01ccfa8e324b41
davideviolante <dav.violante@gmail.com>
2017-03-25 18:38:44 +0100

    Prevent 404 on page refresh

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,8 @@
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as morgan from 'morgan';
-import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
+import * as path from 'path';
 
 import config from './config/db';
 import Cat from './models/cat.model';
@@ -24,6 +25,10 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
   setRoutes(app);
+
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/../../dist/public/index.html'));
+  });
 
   app.listen(app.get('port'), () => {
     console.log('Angular 2 Full Stack listening on port ' + app.get('port'));
