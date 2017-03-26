48bcd875000bcbec7a36bcc431ca2277f3a73c4e
davideviolante <dav.violante@gmail.com>
2017-03-26 22:30:38 +0200

    Migrate from css to scss

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { DataService } from '../services/data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
