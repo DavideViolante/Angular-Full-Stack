118b2e6f75cd2af4bd93df8223c123fbc1e6b8d0
Vladimir Bloshchitsyn <bloshchitsyn@gmail.com>
2017-04-06 22:40:57 +0300

    refactoring: create base controller

---
+++ server/controllers/base.ts
@@ -0,0 +1,55 @@
+abstract class BaseCtrl {
+
+  abstract model: any;
+
+  // Get all
+  getAll = (req, res) => {
+    this.model.find({}, (err, docs) => {
+      if (err) { return console.error(err); }
+      res.json(docs);
+    });
+  };
+
+  // Count all
+  count = (req, res) => {
+    this.model.count((err, count) => {
+      if (err) { return console.error(err); }
+      res.json(count);
+    });
+  };
+
+  // Insert
+  insert = (req, res) => {
+    const obj = new this.model(req.body);
+    obj.save((err, item) => {
+      if (err) { return console.error(err); }
+      res.status(200).json(item);
+    });
+  };
+
+  // Get by id
+  get = (req, res) => {
+    this.model.findOne({ _id: req.params.id }, (err, obj) => {
+      if (err) { return console.error(err); }
+      res.json(obj);
+    });
+  };
+
+  // Update by id
+  update = (req, res) => {
+    this.model.findOneAndUpdate({ _id: req.params.id }, req.body, (err) => {
+      if (err) { return console.error(err); }
+      res.sendStatus(200);
+    });
+  };
+
+  // Delete by id
+  delete = (req, res) => {
+    this.model.findOneAndRemove({ _id: req.params.id }, (err) => {
+      if (err) { return console.error(err); }
+      res.sendStatus(200);
+    });
+  }
+}
+
+export default BaseCtrl;
--- server/controllers/cats.ts
+++ server/controllers/cats.ts
@@ -1,53 +1,8 @@
 import Cat from '../models/cat.model';
+import BaseCtrl from './base';
 
-export default class CatsCtrl {
-
-  // Get all
-  getAll(req, res) {
-    Cat.find({}, (err, docs) => {
-      if (err) { return console.error(err); }
-      res.json(docs);
-    });
-  }
-
-  // Count all
-  count(req, res) {
-    Cat.count((err, count) => {
-      if (err) { return console.error(err); }
-      res.json(count);
-    });
-  }
-
-  // Insert
-  insert(req, res) {
-    const obj = new Cat(req.body);
-    obj.save((err, cat) => {
-      if (err) { return console.error(err); }
-      res.status(200).json(cat);
-    });
-  }
-
-  // Get by id
-  get(req, res) {
-    Cat.findOne({ _id: req.params.id }, (err, obj) => {
-      if (err) { return console.error(err); }
-      res.json(obj);
-    });
-  }
-
-  // Update by id
-  update(req, res) {
-    Cat.findOneAndUpdate({ _id: req.params.id }, req.body, (err) => {
-      if (err) { return console.error(err); }
-      res.sendStatus(200);
-    });
-  }
-
-  // Delete by id
-  delete(req, res) {
-    Cat.findOneAndRemove({ _id: req.params.id }, (err) => {
-      if (err) { return console.error(err); }
-      res.sendStatus(200);
-    });
-  }
+export default class CatsCtrl extends BaseCtrl {
+  model = Cat;
 }
+
+
