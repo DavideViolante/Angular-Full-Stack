3972663a6fedf1fe2cc98969b6a3527ab8f00db8
Jhommark Macasaet <jhommark@gmail.com>
2017-06-10 19:16:24 +0800

    Implement one time setting of routes prefix

--- server/routes.ts
+++ server/routes.ts
@@ -7,24 +7,30 @@ import User from './models/user';
 
 export default function setRoutes(app) {
 
+  // get an instance of the router for api routes
+  const apiRoutes = express.Router();
+
   const catCtrl = new CatCtrl();
   const userCtrl = new UserCtrl();
 
   // Cats
-  app.route('/api/cats').get(catCtrl.getAll);
-  app.route('/api/cats/count').get(catCtrl.count);
-  app.route('/api/cat').post(catCtrl.insert);
-  app.route('/api/cat/:id').get(catCtrl.get);
-  app.route('/api/cat/:id').put(catCtrl.update);
-  app.route('/api/cat/:id').delete(catCtrl.delete);
+  apiRoutes.route('/cats').get(catCtrl.getAll);
+  apiRoutes.route('/cats/count').get(catCtrl.count);
+  apiRoutes.route('/cat').post(catCtrl.insert);
+  apiRoutes.route('/cat/:id').get(catCtrl.get);
+  apiRoutes.route('/cat/:id').put(catCtrl.update);
+  apiRoutes.route('/cat/:id').delete(catCtrl.delete);
 
   // Users
-  app.route('/api/login').post(userCtrl.login);
-  app.route('/api/users').get(userCtrl.getAll);
-  app.route('/api/users/count').get(userCtrl.count);
-  app.route('/api/user').post(userCtrl.insert);
-  app.route('/api/user/:id').get(userCtrl.get);
-  app.route('/api/user/:id').put(userCtrl.update);
-  app.route('/api/user/:id').delete(userCtrl.delete);
+  apiRoutes.route('/login').post(userCtrl.login);
+  apiRoutes.route('/users').get(userCtrl.getAll);
+  apiRoutes.route('/users/count').get(userCtrl.count);
+  apiRoutes.route('/user').post(userCtrl.insert);
+  apiRoutes.route('/user/:id').get(userCtrl.get);
+  apiRoutes.route('/user/:id').put(userCtrl.update);
+  apiRoutes.route('/user/:id').delete(userCtrl.delete);
+
+  // apply the routes to our application with the prefix /api
+  app.use('/api', apiRoutes);
 
 }
