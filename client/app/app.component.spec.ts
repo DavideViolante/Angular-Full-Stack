7883e6e76e8a7896eaf574833c7f985327bdf42e
DavideViolante <dav.violante@gmail.com>
2017-10-22 23:40:12 +0200

    Add tests for AppComponent

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -3,33 +3,85 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
 
 describe('Component: App', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  beforeEach( async(() => {
+  let authService: AuthService;
+  let authServiceStub: {
+    loggedIn: boolean,
+    isAdmin: boolean,
+    currentUser: any
+  };
+
+  beforeEach(async(() => {
+    authServiceStub = {
+      loggedIn: false,
+      isAdmin: false,
+      currentUser: { username: 'Tester' }
+    };
     TestBed.configureTestingModule({
       declarations: [ AppComponent ],
+      providers: [ { provide: AuthService, useValue: authServiceStub } ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents().then(() => {
       fixture = TestBed.createComponent(AppComponent);
       component = fixture.componentInstance;
+      authService = fixture.debugElement.injector.get(AuthService);
       fixture.detectChanges();
     });
   }));
 
-  /*it('should create the app', async(() => {
+  it('should create the app', async(() => {
     expect(component).toBeTruthy();
   }));
 
-  it('should display the navigation bar correctly', () => {
+  it('should display the navigation bar correctly for guests', () => {
     const de = fixture.debugElement.queryAll(By.css('a'));
-    expect(de.length).toBe(3);
+    expect(de.length).toBe(4);
     expect(de[0].nativeElement.textContent).toContain('Home');
-    expect(de[1].nativeElement.textContent).toContain('About');
+    expect(de[1].nativeElement.textContent).toContain('Cats');
+    expect(de[2].nativeElement.textContent).toContain('Login');
+    expect(de[3].nativeElement.textContent).toContain('Register');
     expect(de[0].attributes['routerLink']).toBe('/');
-    expect(de[1].attributes['routerLink']).toBe('/about');
-  });*/
+    expect(de[1].attributes['routerLink']).toBe('/cats');
+    expect(de[2].attributes['routerLink']).toBe('/login');
+    expect(de[3].attributes['routerLink']).toBe('/register');
+  });
+
+  it('should display the navigation bar correctly for logged users', () => {
+    authService.loggedIn = true;
+    fixture.detectChanges();
+    const de = fixture.debugElement.queryAll(By.css('a'));
+    expect(de.length).toBe(4);
+    expect(de[0].nativeElement.textContent).toContain('Home');
+    expect(de[1].nativeElement.textContent).toContain('Cats');
+    expect(de[2].nativeElement.textContent).toContain('Account (Tester)');
+    expect(de[3].nativeElement.textContent).toContain('Logout');
+    expect(de[0].attributes['routerLink']).toBe('/');
+    expect(de[1].attributes['routerLink']).toBe('/cats');
+    expect(de[2].attributes['routerLink']).toBe('/account');
+    expect(de[3].attributes['routerLink']).toBe('/logout');
+  });
+
+  it('should display the navigation bar correctly for admin users', () => {
+    authService.loggedIn = true;
+    authService.isAdmin = true;
+    fixture.detectChanges();
+    const de = fixture.debugElement.queryAll(By.css('a'));
+    expect(de.length).toBe(5);
+    expect(de[0].nativeElement.textContent).toContain('Home');
+    expect(de[1].nativeElement.textContent).toContain('Cats');
+    expect(de[2].nativeElement.textContent).toContain('Account (Tester)');
+    expect(de[3].nativeElement.textContent).toContain('Admin');
+    expect(de[4].nativeElement.textContent).toContain('Logout');
+    expect(de[0].attributes['routerLink']).toBe('/');
+    expect(de[1].attributes['routerLink']).toBe('/cats');
+    expect(de[2].attributes['routerLink']).toBe('/account');
+    expect(de[3].attributes['routerLink']).toBe('/admin');
+    expect(de[4].attributes['routerLink']).toBe('/logout');
+  });
 
 });
