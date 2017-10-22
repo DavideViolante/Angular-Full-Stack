11786d656c867d8b498458f7b7d4782b5c8367fc
DavideViolante <dav.violante@gmail.com>
2017-10-23 00:38:07 +0200

    Add tests for LogoutComponent

--- client/app/logout/logout.component.spec.ts
+++ client/app/logout/logout.component.spec.ts
@@ -1,14 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
+import { AuthService } from '../services/auth.service';
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
   let fixture: ComponentFixture<LogoutComponent>;
+  let authService: AuthService;
+  let authServiceStub: {
+    loggedIn: boolean,
+    logout: any
+  };
 
   beforeEach(async(() => {
+    authServiceStub = {
+      loggedIn: true,
+      logout: (function() {
+        this.loggedIn = false;
+      })
+    };
     TestBed.configureTestingModule({
-      declarations: [ LogoutComponent ]
+      declarations: [ LogoutComponent ],
+      providers: [ { provide: AuthService, useValue: authServiceStub } ],
     })
     .compileComponents();
   }));
@@ -16,10 +29,18 @@ describe('LogoutComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LogoutComponent);
     component = fixture.componentInstance;
+    authService = fixture.debugElement.injector.get(AuthService);
     fixture.detectChanges();
   });
 
-  /*it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
+
+  it('should logout the user', () => {
+    authService.loggedIn = true;
+    expect(authService.loggedIn).toBeTruthy();
+    authService.logout();
+    expect(authService.loggedIn).toBeFalsy();
+  });
 });
