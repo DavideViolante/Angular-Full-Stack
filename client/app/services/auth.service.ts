b025cf97d7e11cf2043b7d3e13d43c4798c55eb2
DavideViolante <dav.violante@gmail.com>
2018-02-18 17:04:14 +0100

    Remove unused iimport

--- client/app/services/auth.service.ts
+++ client/app/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 import { JwtHelperService } from '@auth0/angular-jwt';
 
