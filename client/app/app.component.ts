b46b5e8e2ab2fac2c5487a04c46c0d816b8f5c47
DavideViolante <dav.violante@gmail.com>
2018-02-18 17:11:59 +0100

    Fix #105

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService,
+              private changeDetector: ChangeDetectorRef) { }
+
+  // This fixes: https://github.com/DavideViolante/Angular-Full-Stack/issues/105
+  ngAfterViewChecked() {
+    this.changeDetector.detectChanges();
+  }
 
 }
