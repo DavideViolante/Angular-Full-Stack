492627cba42509e01f924348b2d14a8a0ea8d871
Md. Shohel Rana <cse_shohel@yahoo.com>
2018-03-13 06:43:05 +0600

    Update app.error-handler.ts

--- client/app/app.error-handler.ts
+++ client/app/app.error-handler.ts
@@ -1,6 +1,6 @@
 import { ErrorHandler } from '@angular/core';
 
-export default class AppErrorHandler extends ErrorHandler {
+export class AppErrorHandler extends ErrorHandler {
   constructor() {
     // We rethrow exceptions, so operations like 'bootstrap' will result in an error
     // when an error happens. If we do not rethrow, bootstrap will always succeed.
