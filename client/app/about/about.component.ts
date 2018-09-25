5d02cd6883e9c0abbe838c3af87f887e844dabfd
DavideViolante <dav.violante@gmail.com>
2018-09-25 20:55:12 +0200

    Improve readability and indentation of html files

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html'
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
 
