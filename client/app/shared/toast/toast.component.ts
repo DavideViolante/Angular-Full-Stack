fbf8c8883631008252f4c1f58365885c69e430b5
DavideViolante <dav.violante@gmail.com>
2018-09-25 21:26:13 +0200

    Add sass-lint and rename files from css to scss

--- client/app/shared/toast/toast.component.ts
+++ client/app/shared/toast/toast.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
-  styleUrls: ['./toast.component.css']
+  styleUrls: ['./toast.component.scss']
 })
 export class ToastComponent {
   @Input() message = { body: '', type: '' };
