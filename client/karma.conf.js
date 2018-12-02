f360d0d6d7cdf36924469a9be4b4aa239331efa2
DavideViolante <dav.violante@gmail.com>
2018-12-02 18:39:24 +0100

    Upgrade to angular v7.1.0

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
