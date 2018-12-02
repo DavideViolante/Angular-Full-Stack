0c88b4af28542348d5f6c4f78223be50c88cfad4
DavideViolante <dav.violante@gmail.com>
2018-12-02 18:56:56 +0100

    Add missing spaces in imports

--- client/app/services/auth-guard-admin.service.ts
+++ client/app/services/auth-guard-admin.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
-import {CanActivate} from '@angular/router';
-import {AuthService} from './auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuardAdmin implements CanActivate {
