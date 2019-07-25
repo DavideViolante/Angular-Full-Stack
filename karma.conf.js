808894bff750318a8db3103e578bd750250c398b
DavideViolante <dav.violante@gmail.com>
2019-07-25 21:22:06 +0200

    Fix project name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/cli812'),
+      dir: require('path').join(__dirname, './coverage/angular2-full-stack'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
