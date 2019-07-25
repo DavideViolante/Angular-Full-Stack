3411558fe2ce1dbf1edb3135caca45058b5daf1a
DavideViolante <dav.violante@gmail.com>
2019-07-25 21:35:42 +0200

    Update all other deps

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,7 @@ import * as path from 'path';
 import setRoutes from './routes';
 
 const app = express();
-dotenv.load({ path: '.env' });
+dotenv.config();
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, '../public')));
