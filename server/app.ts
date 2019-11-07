0d203a446049196743013db7677b1c1f01ab2e46
DavideViolante <fungtq3@gmail.com>
2019-11-07 12:35:36 +0100

    Fix deprecation warnings for mongodb

--- server/app.ts
+++ server/app.ts
@@ -23,6 +23,9 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect(mongodbURI)
   .then(db => {
     console.log('Connected to MongoDB');
