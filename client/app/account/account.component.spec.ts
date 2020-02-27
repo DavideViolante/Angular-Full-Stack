aeec996ac9afc43c32b7e13ee12eb4ab072f9727
DavideViolante <fungtq3@gmail.com>
2020-02-27 13:03:06 +0100

    Add unit test to account component

--- client/app/account/account.component.spec.ts
+++ client/app/account/account.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AccountComponent } from './account.component';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
+import { ToastComponent } from '../shared/toast/toast.component';
+import { of } from 'rxjs';
 
-describe('AccountComponent', () => {
+class AuthServiceMock { }
+
+class UserServiceMock {
+  mockUser = {
+    username: 'Test user',
+    email: 'test@example.com',
+    role: 'user'
+  };
+  getUser() {
+    return of(this.mockUser);
+  }
+}
+
+describe('Component: Account', () => {
   let component: AccountComponent;
   let fixture: ComponentFixture<AccountComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccountComponent ]
+      imports: [ FormsModule ],
+      declarations: [ AccountComponent ],
+      providers: [
+        ToastComponent,
+        { provide: AuthService, useClass: AuthServiceMock },
+        { provide: UserService, useClass: UserServiceMock },
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +42,32 @@ describe('AccountComponent', () => {
     fixture = TestBed.createComponent(AccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.user = {
+      username: 'Test user',
+      email: 'test@example.com'
+    };
+    fixture.detectChanges();
   });
 
-  /*it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
+
+  it('should display the page header text', () => {
+    const el = fixture.debugElement.query(By.css('h4')).nativeElement;
+    expect(el.textContent).toContain('Account settings');
+  });
+
+  it('should display the username and email inputs filled', async () => {
+    await fixture.whenStable();
+    const [usernameInput, emailInput] = fixture.debugElement.queryAll(By.css('input'));
+    expect(usernameInput.nativeElement.value).toContain('Test user');
+    expect(emailInput.nativeElement.value).toContain('test@example.com');
+  });
+
+  it('should display the save button and be enabled', () => {
+    const saveBtn = fixture.debugElement.query(By.css('button'));
+    expect(saveBtn.nativeElement.disabled).toBeFalsy();
+  });
+
 });
