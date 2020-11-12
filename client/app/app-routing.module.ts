7d6557cd35ddd51e385bb89da4a1e710980c7dd7
DavideViolante <fungtq3@gmail.com>
2020-11-12 19:14:58 +0100

    Upgrade to Angular v11

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 
