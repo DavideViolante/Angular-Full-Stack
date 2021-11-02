fbb9726c78d251cfe18a3b84137e6feda3936da5
DavideViolante <fungtq3@gmail.com>
2021-11-02 15:07:53 +0100

    Update angular to v12.2.12 Fix mongoose deprecations

--- server/mongo.ts
+++ server/mongo.ts
@@ -7,11 +7,6 @@ async function setMongo(): Promise<any> {
   } else {
     mongodbURI = process.env.MONGODB_URI;
   }
-  mongoose.set('useCreateIndex', true);
-  mongoose.set('useNewUrlParser', true);
-  mongoose.set('useFindAndModify', false);
-  mongoose.set('useUnifiedTopology', true);
-  // Connect to MongoDB using Mongoose
   await mongoose.connect(mongodbURI);
   console.log('Connected to MongoDB');
 }
