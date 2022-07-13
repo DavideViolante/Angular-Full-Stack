9cf67e85f98836c6f74c6831a9b035d686d37e17
Thavarajan.M <thavarajan@gmail.com>
2022-07-13 07:19:05 +0530

    refactor cat service

--- client/app/services/cat.service.ts
+++ client/app/services/cat.service.ts
@@ -1,36 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { Cat } from '../shared/models/cat.model';
+import { EntityService } from './base.service';
 
 @Injectable()
-export class CatService {
-
-  constructor(private http: HttpClient) { }
-
-  getCats(): Observable<Cat[]> {
-    return this.http.get<Cat[]>('/api/cats');
-  }
-
-  countCats(): Observable<number> {
-    return this.http.get<number>('/api/cats/count');
-  }
-
-  addCat(cat: Cat): Observable<Cat> {
-    return this.http.post<Cat>('/api/cat', cat);
-  }
-
-  getCat(cat: Cat): Observable<Cat> {
-    return this.http.get<Cat>(`/api/cat/${cat._id}`);
-  }
-
-  editCat(cat: Cat): Observable<any> {
-    return this.http.put(`/api/cat/${cat._id}`, cat, { responseType: 'text' });
-  }
-
-  deleteCat(cat: Cat): Observable<any> {
-    return this.http.delete(`/api/cat/${cat._id}`, { responseType: 'text' });
-  }
-
+export class CatService extends EntityService<Cat>{
+  SingularAPI = 'cat';
+  PluralAPI = 'cats';
 }
