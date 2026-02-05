a46bc2f7c14b935a68f9b818bce826114e20f5a8
DavideViolante <fungtq3@gmail.com>
2026-02-05 18:10:24 +0100

    test: refactor tests, add account component tests

--- client/app/about/about.component.spec.ts
+++ client/app/about/about.component.spec.ts
@@ -1,24 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
 describe('Component: About', () => {
+  let fixture: ComponentFixture<AboutComponent>;
+  let compiled: HTMLElement;
+  
   beforeEach(async() => {
     await TestBed.configureTestingModule({
       imports: [AboutComponent]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AboutComponent);
+    await fixture.whenStable();
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the about component', () => {
-    const fixture = TestBed.createComponent(AboutComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render the header', async () => {
-    const fixture = TestBed.createComponent(AboutComponent);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.card-header')?.textContent).toContain('About');
   });
 });
--- client/app/account/account.component.spec.ts
+++ client/app/account/account.component.spec.ts
@@ -1,15 +1,12 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { Observable, of } from 'rxjs';
 
-import { ToastComponent } from '../shared/toast/toast.component';
-import { User } from '../shared/models/user.model';
+import { AccountComponent } from './account.component';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
-import { AccountComponent } from './account.component';
-import { of, Observable } from 'rxjs';
-
-class AuthServiceMock { }
+import { ToastComponent } from '../shared/toast/toast.component';
+import { User } from '../shared/models/user.model';
 
 class UserServiceMock {
   mockUser = {
@@ -23,56 +20,43 @@ class UserServiceMock {
 }
 
 describe('Component: Account', () => {
-  let component: AccountComponent;
   let fixture: ComponentFixture<AccountComponent>;
   let compiled: HTMLElement;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [ FormsModule ],
-      declarations: [ AccountComponent ],
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [AccountComponent],
       providers: [
+        AuthService,
         ToastComponent,
-        { provide: AuthService, useClass: AuthServiceMock },
         { provide: UserService, useClass: UserServiceMock },
-      ],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
+        { provide: JWT_OPTIONS, useValue: {} }, JwtHelperService
+      ]
+    }).compileComponents();
+    
     fixture = TestBed.createComponent(AccountComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    component.user = {
-      username: 'Test user',
-      email: 'test@example.com'
-    };
-    fixture.detectChanges();
     compiled = fixture.nativeElement as HTMLElement;
+    await fixture.whenStable();
   });
 
-  it('should create', () => {
+  it('should create the account component', () => {
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
-  it('should display the page header text', () => {
-    const header = compiled.querySelector('.card-header');
-    expect(header?.textContent).toContain('Account settings');
+  it('should render the header', async () => {
+    expect(compiled.querySelector('.card-header')?.textContent).toContain('Account settings');
   });
 
   it('should display the username and email inputs filled', async () => {
-    await fixture.whenStable();
     const inputs = compiled.querySelectorAll('input');
     expect(inputs[0].value).toContain('Test user');
     expect(inputs[1].value).toContain('test@example.com');
   });
 
-  it('should display the save button enabled', () => {
+  it('should display the save button enabled', async () => {
     const button = compiled.querySelector('button');
     expect(button).toBeTruthy();
     expect(button?.disabled).toBeFalsy();
   });
-
 });
